a8362fb3bbf702af22c1254af7c46718f9e8e959
mcrkgus <mcrkgus@naver.com>
2024-05-10 23:28:35 +0900

    [Setting] #1 - Presentation 폴더링

---
+++ KakaoPay-iOS/KakaoPay-iOS/Presentation/Home/View/HomeView.swift
@@ -0,0 +1,49 @@
+//
+//  HomeView.swift
+//  KakaoPay-iOS
+//
+//  Created by Gahyun Kim on 2024/05/10.
+//
+
+import UIKit
+
+final class HomeView: UIView {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - UI Components
+    
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setStyle()
+        setHierarchy()
+        setLayout()
+    
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+private extension HomeView {
+    func setStyle() {
+        
+    }
+    
+    func setHierarchy() {
+
+    }
+    
+    func setLayout() {
+
+    }
+}
---
+++ KakaoPay-iOS/KakaoPay-iOS/Presentation/Home/ViewController/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeViewController.swift
+//  KakaoPay-iOS
+//
+//  Created by Gahyun Kim on 2024/05/10.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - UI Components
+    
+    
+    // MARK: - Life Cycles
+    
+    override func loadView() {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
