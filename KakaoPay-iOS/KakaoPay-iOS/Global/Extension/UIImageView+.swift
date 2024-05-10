8d0975e2dc78a38c1d54698ca52b36ff577b923d
mcrkgus <mcrkgus@naver.com>
2024-05-10 23:44:37 +0900

    [Setting] #1 - SPM 설치

--- KakaoPay-iOS/KakaoPay-iOS/Global/Extension/UIImageView+.swift
+++ KakaoPay-iOS/KakaoPay-iOS/Global/Extension/UIImageView+.swift
@@ -7,19 +7,19 @@
 
 
 import UIKit
-// import Kingfisher
+import Kingfisher
 
 extension UIImageView {
     
     /// url을 문자열로 넘겨주면 kingfisher를 통해 이미지 설정
-//    func kfSetImage(url: String?) {
-//        guard let url = url else { return }
-//        
-//        if let url = URL(string: url) {
-//            kf.indicatorType = .activity
-//            kf.setImage(with: url,
-//                        placeholder: nil,
-//                        options: [.transition(.fade(1.0))], progressBlock: nil)
-//        }
-//    }
+    func kfSetImage(url: String?) {
+        guard let url = url else { return }
+        
+        if let url = URL(string: url) {
+            kf.indicatorType = .activity
+            kf.setImage(with: url,
+                        placeholder: nil,
+                        options: [.transition(.fade(1.0))], progressBlock: nil)
+        }
+    }
 }
--- KakaoPay-iOS/KakaoPay-iOS/Presentation/Home/Cells/HomeCollectionViewCell.swift
+++ KakaoPay-iOS/KakaoPay-iOS/Presentation/Home/Cells/HomeCollectionViewCell.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class HomeCollectionViewCell: UICollectionViewCell {
     
     // MARK: - Properties
