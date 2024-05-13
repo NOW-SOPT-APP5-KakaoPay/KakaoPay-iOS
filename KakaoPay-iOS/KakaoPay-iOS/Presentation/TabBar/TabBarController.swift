c08c821e2ae5acd9ee59d3e5a4573efd38564941
mcrkgus <mcrkgus@naver.com>
2024-05-13 14:15:42 +0900

    [Feat] #5 - TabBar 구현

--- KakaoPay-iOS/KakaoPay-iOS/Presentation/TabBar/TabBarController.swift
+++ KakaoPay-iOS/KakaoPay-iOS/Presentation/TabBar/TabBarController.swift
@@ -5,4 +5,90 @@
 //  Created by Gahyun Kim on 2024/05/13.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+// MARK: - Tab Bar
+
+final class TabBarController: UITabBarController {
+    
+    var customTabBar = CustomTabBar()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setValue(customTabBar, forKey: "tabBar")
+        
+        setupStyle()
+        addTabBarController()
+    }
+}
+
+// MARK: - Private Extensions
+
+private extension TabBarController {
+    func setupStyle() {
+        view.backgroundColor = .kakaoWhite
+        tabBar.backgroundColor = .kakaoWhite
+        tabBar.unselectedItemTintColor = .kakaoGray600
+        tabBar.tintColor = .kakaoBlack
+    }
+    
+    func addTabBarController() {
+        let viewControllers = TabBarItem.allCases.map {
+            let currentViewController = createViewController(
+                title: $0.itemTitle ?? "",
+                image: $0.normalItem ?? UIImage(),
+                selectedImage: $0.selectedItem ?? UIImage(),
+                viewController: $0.targetViewController
+            )
+            return currentViewController
+        }
+        setViewControllers(viewControllers, animated: false)
+    }
+    
+    func createViewController(title: String,
+                              image: UIImage,
+                              selectedImage: UIImage,
+                              viewController: UIViewController) -> UIViewController {
+        let currentViewController = viewController
+        let tabbarItem = UITabBarItem(
+            title: title,
+            image: image.withRenderingMode(.alwaysOriginal),
+            selectedImage: selectedImage.withRenderingMode(.alwaysOriginal)
+        )
+        
+        
+        // title이 선택되지 않았을 때 폰트, 색상 설정
+        let normalAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.appleRegular(size: 12),
+            .foregroundColor: UIColor.kakaoGray600
+        ]
+        
+        // title이 선택되었을 때 폰트, 색상 설정
+        let selectedAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.appleRegular(size: 12),
+            .foregroundColor: UIColor.kakaoBlack
+        ]
+        
+        tabbarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -5)
+        tabbarItem.setTitleTextAttributes(normalAttributes, for: .normal)
+        tabbarItem.setTitleTextAttributes(selectedAttributes, for: .selected)
+        
+        currentViewController.tabBarItem = tabbarItem
+        
+        return currentViewController
+    }
+}
+
+// Custom Tab Bar
+final class CustomTabBar: UITabBar {
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        var size = super.sizeThatFits(size)
+        size.height += 11
+        return size
+    }
+}
