2e77f5c353a194d8737a1fab24ce31dcb7f558d1
mcrkgus <mcrkgus@naver.com>
2024-05-14 12:55:38 +0900

    [Feat] #7 - TransferModel dummy

--- KakaoPay-iOS/KakaoPay-iOS/Presentation/Transfer/Model/TransferModel.swift
+++ KakaoPay-iOS/KakaoPay-iOS/Presentation/Transfer/Model/TransferModel.swift
@@ -5,4 +5,20 @@
 //  Created by Gahyun Kim on 2024/05/13.
 //
 
-import Foundation
+
+struct TransferModel {
+    let bank: String
+    let bankAccount: String
+    let name: String
+    let bookmark: Bool
+}
+
+extension TransferModel {
+    static func dummy() -> [TransferModel] {
+        return [
+        TransferModel(bank: "신한", bankAccount: "1111-1111-1111", name: "김가현", bookmark: false),
+        TransferModel(bank: "토스", bankAccount: "2222-2222-2222", name: "조혜린", bookmark: false),
+        TransferModel(bank: "카카오", bankAccount: "3333-3333-3333", name: "한정원", bookmark: false)
+        ]
+    }
+}
