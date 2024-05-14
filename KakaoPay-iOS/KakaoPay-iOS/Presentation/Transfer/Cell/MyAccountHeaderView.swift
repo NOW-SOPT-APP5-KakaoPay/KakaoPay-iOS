4740ff93973117f52bf2e99ff9c7b9581f4563ab
mcrkgus <mcrkgus@naver.com>
2024-05-14 12:53:48 +0900

    [Feat] #7 - Account Header 구현

--- KakaoPay-iOS/KakaoPay-iOS/Presentation/Transfer/Cell/MyAccountHeaderView.swift
+++ KakaoPay-iOS/KakaoPay-iOS/Presentation/Transfer/Cell/MyAccountHeaderView.swift
@@ -5,4 +5,79 @@
 //  Created by Gahyun Kim on 2024/05/13.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class MyAccountHeaderView: UITableViewHeaderFooterView {
+    
+    // MARK: - UI Components
+    
+    private let myAccountLabel = UILabel()
+    private let countLabel = UILabel()
+    private let arrowButton = UIButton()
+    
+
+    // MARK: - Life Cycle
+    
+    override init(reuseIdentifier: String?) {
+            super.init(reuseIdentifier: reuseIdentifier)
+            setView()
+        }
+        
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+        }
+    
+    func setView() {
+        setupStyle()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: - set up Style
+    
+    private func setupStyle() {
+        myAccountLabel.do {
+            $0.text = "내 계좌"
+            $0.font = .appleRegular(size: 13)
+            $0.textColor = .kakaoBlack
+        }
+        
+        countLabel.do {
+            $0.text = "+1개"
+            $0.font = .appleRegular(size: 13)
+            $0.textColor = .kakaoGray600
+        }
+        
+        arrowButton.do {
+            $0.setImage(.icDown, for: .normal)
+        }
+    }
+    
+    // MARK: - set up Hierarchy
+    
+    private func setupHierarchy() {
+       addSubviews(myAccountLabel, countLabel, arrowButton)
+    }
+    
+    // MARK: - set up Layout
+    
+    private func setupLayout() {
+        myAccountLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(8)
+            $0.leading.equalToSuperview().inset(22)
+        }
+        
+        countLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(9)
+            $0.trailing.equalToSuperview().inset(49)
+        }
+        
+        arrowButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(4)
+            $0.trailing.equalToSuperview().inset(20)
+        }
+    }
+}
