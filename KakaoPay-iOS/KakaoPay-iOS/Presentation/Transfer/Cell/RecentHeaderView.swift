831095d0f58e231d22033b6673a2899c4872d2e4
mcrkgus <mcrkgus@naver.com>
2024-05-14 12:54:06 +0900

    [Feat] #7 - Recent Account Header 구현

--- KakaoPay-iOS/KakaoPay-iOS/Presentation/Transfer/Cell/RecentHeaderView.swift
+++ KakaoPay-iOS/KakaoPay-iOS/Presentation/Transfer/Cell/RecentHeaderView.swift
@@ -5,4 +5,57 @@
 //  Created by Gahyun Kim on 2024/05/13.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class RecentHeaderView: UITableViewHeaderFooterView {
+    
+    // MARK: - UIComponent
+    
+    private let recentLabel = UILabel()
+    
+    
+    // MARK: - Life Cycle
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        setView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setView() {
+        setupStyle()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: - set up Style
+    
+    private func setupStyle() {
+        recentLabel.do {
+            $0.text = "최근"
+            $0.font = .appleRegular(size: 13)
+            $0.textColor = .kakaoBlack
+        }
+    }
+    
+    // MARK: - set up Hierarchy
+    
+    private func setupHierarchy() {
+        addSubview(recentLabel)
+    }
+    
+    // MARK: - set up Layout
+    
+    private func setupLayout() {
+        recentLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(10)
+            $0.leading.equalToSuperview().inset(22)
+        }
+    }
+}
