b231d0b56e38e8e3ee88b8e38f5e5bac4c7681b7
mcrkgus <mcrkgus@naver.com>
2024-05-16 15:23:07 +0900

    [Feat] #10 - Custom Divide View

---
+++ KakaoPay-iOS/KakaoPay-iOS/Presentation/Payment/View/BarcodePaymentView.swift
@@ -0,0 +1,8 @@
+//
+//  BarcodePaymentView.swift
+//  KakaoPay-iOS
+//
+//  Created by Gahyun Kim on 2024/05/16.
+//
+
+import Foundation
---
+++ KakaoPay-iOS/KakaoPay-iOS/Presentation/Payment/View/CustomDivideView.swift
@@ -0,0 +1,23 @@
+//
+//  CustomDivideView.swift
+//  KakaoPay-iOS
+//
+//  Created by Gahyun Kim on 2024/05/16.
+//
+
+import UIKit
+
+class CustomDivideView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .kakaoBluegray400
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
