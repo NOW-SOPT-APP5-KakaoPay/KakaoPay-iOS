79a42e5f2cb8f6b6de9c89ada400c25362a84dac
mcrkgus <mcrkgus@naver.com>
2024-05-20 14:54:17 +0900

    [Feat] #10 - Button addTarget

--- KakaoPay-iOS/KakaoPay-iOS/Presentation/Payment/ViewController/PaymentViewController.swift
+++ KakaoPay-iOS/KakaoPay-iOS/Presentation/Payment/ViewController/PaymentViewController.swift
@@ -24,6 +24,19 @@ final class PaymentViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setaddTarget()
+    }
+}
 
+
+private extension PaymentViewController {
+    func setaddTarget() {
+        rootView.closeButton.addTarget(self, action: #selector(tappedButton), for: .touchUpInside)
+    }
+    
+    @objc
+    func tappedButton() {
+        print("닫기 버튼 클릭")
+        navigationController?.popViewController(animated: false)
     }
 }
