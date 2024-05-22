5f1b3526bb8b3b64577cefcfb58cc289f0873db7
mcrkgus <mcrkgus@naver.com>
2024-05-22 16:49:37 +0900

    [Network] #13 - PayMoneyService (Get)

--- KakaoPay-iOS/KakaoPay-iOS/Network/Service/PayMoneyService.swift
+++ KakaoPay-iOS/KakaoPay-iOS/Network/Service/PayMoneyService.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+import Moya
+
+final class PayMoneyService: APIService {
+    static let shared = PayMoneyService()
+    private var paymoneyProvider = MoyaProvider<BaseAPIService>(plugins: [MoyaLoggingPlugin()])
+    private override init() {}
+}
+
+extension PayMoneyService {
+    func getPayMoneyAPI(completion: @escaping (NetworkResult<Any>) -> Void) {
+        paymoneyProvider.request(.getPayMoney) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                let networkResult = self.judgeStatus(by: statusCode, data, GetPayMoneyDTO.self)
+                completion(networkResult)
+                
+            case .failure:
+                completion(.networkFail)
+            }
+        }
+    }
+}
