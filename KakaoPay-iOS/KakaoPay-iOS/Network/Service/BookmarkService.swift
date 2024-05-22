43f5557d10bbdfb7a88f047733bf6838688d5573
mcrkgus <mcrkgus@naver.com>
2024-05-22 16:49:11 +0900

    [Network] #13 - BookmarkService (Post, Delete)

--- KakaoPay-iOS/KakaoPay-iOS/Network/Service/BookmarkService.swift
+++ KakaoPay-iOS/KakaoPay-iOS/Network/Service/BookmarkService.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+import Moya
+
+final class BookmarkService: APIService {
+    static let shared = BookmarkService()
+    private var bookmarkProvider = MoyaProvider<BaseAPIService>(plugins: [MoyaLoggingPlugin()])
+    private override init() {}
+}
+
+extension BookmarkService {
+    func postBookmarkAPI(bank: String, bankAccount: String, completion: @escaping (NetworkResult<Any>) -> Void) {
+        bookmarkProvider.request(.postBookmark(bank: bank, bankAccount: bankAccount)) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                let networkResult = self.judgeStatus(by: statusCode, data, PostBookmarkDTO.self)
+                completion(networkResult)
+                
+            case .failure:
+                completion(.networkFail)
+            }
+        }
+    }
+    
+    func deleteBookmarkAPI(bank: String, bankAccount: String, completion: @escaping (NetworkResult<Any>) -> Void) {
+        bookmarkProvider.request(.deleteBookmark(bank: bank, bankAccount: bankAccount)) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                let networkResult = self.judgeStatus(by: statusCode, data, DeleteBookmarkDTO.self)
+                completion(networkResult)
+                
+            case .failure:
+                completion(.networkFail)
+            }
+        }
+    }
+}
